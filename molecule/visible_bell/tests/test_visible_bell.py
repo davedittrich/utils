ed356fb96d8bef4335949bb1cd1438e0a697f25f
Dave Dittrich <dave.dittrich@gmail.com>
2021-07-19 15:02:03 -0700

    Support more users for visible_bell configuration

--- molecule/visible_bell/tests/test_visible_bell.py
+++ molecule/visible_bell/tests/test_visible_bell.py
@@ -3,38 +3,38 @@ import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
+test_user = 'root'
 
 def test_inputrc(host):
-    f = host.file(os.path.join(host.user().home, '.inputrc'))
+    f = host.file(os.path.join(host.user(test_user).home, '.inputrc'))
 
     assert f.exists
     assert f.content_string.find(r'set prefer-visible-bell') > -1
 
 
 def test_bashrc(host):
-    f = host.file(os.path.join(host.user().home, '.bashrc'))
+    f = host.file(os.path.join(host.user(test_user).home, '.bashrc'))
 
     assert f.exists
     assert f.content_string.find(r'set bellstyle visible') > -1
 
 
 def test_cshrc(host):
-    f = host.file(os.path.join(host.user().home, '.cshrc'))
+    f = host.file(os.path.join(host.user(test_user).home, '.cshrc'))
 
     assert f.exists
     assert f.content_string.find(r'set visiblebell') > -1
 
 
 def test_exrc(host):
-    f = host.file(os.path.join(host.user().home, '.exrc'))
+    f = host.file(os.path.join(host.user(test_user).home, '.exrc'))
 
     assert f.exists
     assert r'set flash' in f.content_string
 
 
 def test_vimrc(host):
-    f = host.file(os.path.join(host.user().home, '.vimrc'))
+    f = host.file(os.path.join(host.user(test_user).home, '.vimrc'))
 
     assert f.exists
     assert r'set vb t_vb=' in f.content_string
