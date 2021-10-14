e582f88fcf456ae19c8260844524040a991f9a6a
Dave Dittrich <dave.dittrich@gmail.com>
2021-10-13 19:41:33 -0700

    Paramatrize config tests with fixtures

--- molecule/shared/tests/test_branding.py
+++ molecule/shared/tests/test_branding.py
@@ -96,19 +96,6 @@ def test_user_wallpaper_setting(host, user):
 
 
 @skip_unless_role('davedittrich.utils.branding')
-@pytest.mark.parametrize('user', ansible_vars['accounts'])
-def test_LXDE_autostart_xset(host, user):
-    homedir = get_homedir(host=host, user=user)
-    f = host.file(
-        os.path.join(homedir, '.config/lxsession/LXDE/autostart')
-    )
-    assert f.exists
-    assert f.user == user
-    assert f.group == user
-    assert r'xset' in f.content_string
-
-
-@skip_unless_role('davedittrich.utils.branding')
 def test_lightdm_login_background(host):
     assert 'lxde_wallpapers_directory' in ansible_vars
     wallpapers_dir = ansible_vars['lxde_wallpapers_directory']
@@ -120,18 +107,44 @@ def test_lightdm_login_background(host):
     assert f'background={login_background}' in f.content_string
 
 
+# @skip_unless_role('davedittrich.utils.branding')
+# @pytest.mark.parametrize('user', ansible_vars['accounts'])
+# def test_LXDE_desktop_conf(host, user):
+#     homedir = get_homedir(host=host, user=user)
+#     f = host.file(
+#         os.path.join(homedir, '.config/lxsession/LXDE/desktop.conf')
+#     )
+#     assert f.exists
+#     assert f.user == user
+#     assert f.group == user
+#     assert r'Beep=0' in f.content_string
+#     assert r'menu_prefix=lxde-' in f.content_string
+
+# Use one fixture to return the list of users, and another to return the
+# list of configuration files.  These are used to generate test functions
+# at collection time. For more information on how this works, see:
+#   https://medium.com/opsops/deepdive-into-pytest-parametrization-cb21665c05b9
+
+@pytest.fixture(params=ansible_vars['accounts'])
+def fixture_users(request):
+    return request.param
+
+
+@pytest.fixture(params=ansible_vars['config_templates'])
+def fixture_config_files(request):
+    return str(request.param).replace('.j2', '')
+
+
 @skip_unless_role('davedittrich.utils.branding')
-@pytest.mark.parametrize('user', ansible_vars['accounts'])
-def test_LXDE_desktop_conf(host, user):
+def test_config_files(host, fixture_users, fixture_config_files):
+    user = fixture_users
+    config_file = fixture_config_files
     homedir = get_homedir(host=host, user=user)
-    f = host.file(
-        os.path.join(homedir, '.config/lxsession/LXDE/desktop.conf')
-    )
+    config_path = os.path.join(homedir, '.config', config_file)
+    f = host.file(config_path)
     assert f.exists
     assert f.user == user
     assert f.group == user
-    assert r'Beep=0' in f.content_string
-    assert r'menu_prefix=lxde-' in f.content_string
 
 
 # vim: set fileencoding=utf-8 ts=4 sw=4 tw=0 et :
