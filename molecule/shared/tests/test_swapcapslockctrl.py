e4aefe3227f6851223c2aee1437076669b7d2182
Dave Dittrich <dave.dittrich@gmail.com>
2021-10-27 23:30:58 -0700

    Stop using `udev` for key remapping
    This triggers a bug with Raspbian (HypriotOS)

--- molecule/shared/tests/test_swapcapslockctrl.py
+++ molecule/shared/tests/test_swapcapslockctrl.py
@@ -6,19 +6,6 @@ from molecule.shared import (
 
 
 @skip_unless_role('davedittrich.utils.swapcapslockctrl')
-def test_udev_package(host):
-    assert host.package('udev').is_installed
-
-
-@skip_unless_role('davedittrich.utils.swapcapslockctrl')
-def test_udev_hwdb_file(host):
-    f = host.file('/etc/udev/hwdb.d/99-swapcapslockctrl.hwdb')
-    assert f.exists
-    assert f.user == 'root'
-    assert r'KEYBOARD_KEY_700E0=capslock' in f.content_string
-
-
-@skip_unless_role('davedittrich.utils.swapcapslockctrl')
 def test_xkboptions(host):
     f = host.file('/etc/default/keyboard')
     assert f.exists
@@ -31,5 +18,6 @@ def test_keyboard_configuration(host):
     with host.sudo():
         result = host.check_output("debconf-show keyboard-configuration")
         assert r'ctrl:swapcaps' in result
+
 
 # vim: set fileencoding=utf-8 ts=4 sw=4 tw=0 et :
