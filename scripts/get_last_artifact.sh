d59b454cf56c6b2c86739dc918480a0922468b90
Dave Dittrich <dave.dittrich@gmail.com>
2022-09-18 21:57:38 -0700

    Add code to specify artifact directory

--- scripts/get_last_artifact.sh
+++ scripts/get_last_artifact.sh
@@ -5,7 +5,18 @@
 # assumes the artifacts are located in the current working directory,
 # as would occur when run from the `Makefile` in the repo root directory.
 
-last_artifact=$(ls davedittrich-utils-*.tar.gz 2>/dev/null | sort -r | head -n 1)
+if [[ ! -z "$1" ]]; then
+  if [[ ! -d "$1" ]]; then
+    echo "[-] directory not found: $1"
+  fi
+  dir="${1}"
+  if grep -q '/$' <<< "${1}"; then
+    dir="$(sed 's/\/$//' <<< \"${1})\""
+  fi
+else
+  dir="."
+fi
+last_artifact=$(ls ${dir}/davedittrich-utils-*.tar.gz 2>/dev/null | sort -r | head -n 1)
 if [[ -z "${last_artifact}" ]]; then
   echo "None"
 else
