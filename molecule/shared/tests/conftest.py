9809d9f76f660f87960336519823548371cf8b58
Dave Dittrich <dave.dittrich@gmail.com>
2024-11-20 16:27:53 -0800

    Fix feedback on runtime vars file lost during test

--- molecule/shared/tests/conftest.py
+++ molecule/shared/tests/conftest.py
@@ -4,7 +4,10 @@
 Pytest testing configuration.
 """
 
-from helpers import get_testinfra_hosts
+from helpers import (
+    get_ansible_vars_file,
+    get_testinfra_hosts,
+)
 
 
 # def pytest_collectreport(report):
@@ -15,6 +18,7 @@ def pytest_sessionstart(session):
     print("[+] pytest_sessionstart() has been called")
     if len(get_testinfra_hosts()) == 0:
         raise RuntimeError("[-] no hosts found")
+    print(f"[+] using cached Ansible runtime variables from 'converge' stored in '{get_ansible_vars_file()}'")
 
 
 # vim: set ts=4 sw=4 tw=0 et :
